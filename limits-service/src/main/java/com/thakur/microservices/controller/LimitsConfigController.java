2275eeecb658c64b3564acbfb0125df2fe74e5a0
thakur-singh21 <thakur.singh21@gmail.com>
2020-04-13 13:14:28 +0530

    adding clod config server

--- limits-service/src/main/java/com/thakur/microservices/controller/LimitsConfigController.java
+++ limits-service/src/main/java/com/thakur/microservices/controller/LimitsConfigController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.thakur.microservices.config.LimitsConfiguration;
 
 @RestController
